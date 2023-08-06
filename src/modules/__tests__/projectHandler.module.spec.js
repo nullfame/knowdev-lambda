cb2eba0449a03d2bed243dd4cfd5e8db7bc6db6b
Adam Finlayson <amf@adamfinlayson.com>
2023-08-05 21:11:55 -0500

    feat: project handler module

--- src/modules/__tests__/projectHandler.module.spec.js
+++ src/modules/__tests__/projectHandler.module.spec.js
@@ -1,4 +1,35 @@
+const { NotFoundError } = require("@knowdev/errors");
+const HTTP = require("@knowdev/http");
+
+const { matchers } = require("jest-json-schema");
+
 const projectHandler = require("../projectHandler.module");
+
+//
+//
+// Configuration
+//
+
+expect.extend(matchers);
+
+const jsonApiErrorSchema = {
+  type: "object",
+  properties: {
+    errors: {
+      type: "array",
+      items: {
+        type: "object",
+        properties: {
+          status: { type: "number" },
+          title: { type: "string" },
+          detail: { type: "string" },
+        },
+        required: ["status", "title"],
+      },
+    },
+  },
+  required: ["errors"],
+};
 
 //
 //
@@ -15,9 +46,18 @@ const projectHandler = require("../projectHandler.module");
 // Mock environment
 //
 
+let mockContext;
+let mockEvent;
+
 const DEFAULT_ENV = process.env;
 beforeEach(() => {
   process.env = { ...process.env };
+  mockContext = {
+    context: "MOCK_CONTEXT",
+  };
+  mockEvent = {
+    event: "MOCK_EVENT",
+  };
 });
 afterEach(() => {
   process.env = DEFAULT_ENV;
@@ -28,10 +68,88 @@ afterEach(() => {
 // Run tests
 //
 
-describe("ProjectHandler module", () => {
+describe("Project handler module", () => {
   it("Works", async () => {
-    const response = await projectHandler();
-    console.log("response :>> ", response);
-    expect(response).not.toBeUndefined();
+    expect(projectHandler).toBeDefined();
+    expect(projectHandler).toBeFunction();
+  });
+  it("Produces a function", () => {
+    const callback = jest.fn();
+    const handler = projectHandler(callback);
+    expect(handler).toBeFunction();
+  });
+  it("Calls a function I pass it", () => {
+    const callback = jest.fn();
+    const handler = projectHandler(callback);
+    handler(mockEvent, mockContext, 3, 4, 5);
+    expect(callback).toHaveBeenCalledTimes(1);
+    expect(callback).toHaveBeenCalledWith(mockEvent, mockContext, 3, 4, 5);
+  });
+  describe("Error handling", () => {
+    it("Will catch an unhandled thrown error", () => {
+      const mockFunction = jest.fn(() => {
+        throw new Error("Sorpresa!");
+      });
+      const handler = projectHandler(mockFunction);
+      const response = handler(mockEvent, mockContext);
+      expect(mockFunction).toHaveBeenCalledTimes(1);
+      expect(response.isProjectError).toBeTrue();
+      // expect(response).toMatchSchema(jsonApiErrorSchema);
+      // expect(response.errors[0].status).toBe(500);
+      // The response title will be "Internal Application Error" but we don't want to test that here
+      // expect(response.errors[0].title).toBe("Internal Application Error");
+    });
+    it("Will catch a thrown ProjectError and respond with the correct status code", () => {
+      // Mock a function that throws NotFoundError
+      const mockFunction = jest.fn(() => {
+        throw new NotFoundError();
+      });
+      const handler = projectHandler(mockFunction);
+      const response = handler(mockEvent, mockContext);
+      expect(response.isProjectError).toBeTrue();
+      expect(response.status).toBe(404);
+    });
+  });
+  describe("Unavailable mode", () => {
+    it("Works as normal when process.env.PROJECT_UNAVAILABLE is set to false", () => {
+      process.env.PROJECT_UNAVAILABLE = "false";
+      const mockFunction = jest.fn();
+      const handler = projectHandler(mockFunction);
+      handler(mockEvent, mockContext);
+      expect(mockFunction).toHaveBeenCalledTimes(1);
+    });
+    it("Will respond with a 503 if process.env.PROJECT_UNAVAILABLE is set to true", () => {
+      process.env.PROJECT_UNAVAILABLE = "true";
+      const mockFunction = jest.fn();
+      const handler = projectHandler(mockFunction);
+      const response = handler(mockEvent, mockContext);
+      expect(response.isProjectError).toBeTrue();
+      expect(response.status).toBe(HTTP.CODE.UNAVAILABLE);
+      expect(mockFunction).not.toHaveBeenCalled();
+    });
+    it("Will respond with a 503 if unavailable=true is passed to the handler", () => {
+      const mockFunction = jest.fn();
+      const handler = projectHandler(mockFunction, { unavailable: true });
+      const response = handler(mockEvent, mockContext);
+      expect(response.isProjectError).toBeTrue();
+      expect(response.status).toBe(HTTP.CODE.UNAVAILABLE);
+      expect(mockFunction).not.toHaveBeenCalled();
+    });
+  });
+  describe("API Gateway formatting", () => {
+    it("Will catch an unhandled thrown error", () => {
+      const mockFunction = jest.fn(() => {
+        throw new Error("Sorpresa!");
+      });
+      const handler = projectHandler(mockFunction, { api: true });
+      const response = handler(mockEvent, mockContext);
+      expect(mockFunction).toHaveBeenCalledTimes(1);
+      expect(response.isProjectError).toBeUndefined();
+      expect(response.data).toBeUndefined();
+      expect(response.body).toBeString();
+      expect(JSON.parse(response.body)).toMatchSchema(jsonApiErrorSchema);
+      expect(response.status).toBeUndefined();
+      expect(response.statusCode).toBe(500);
+    });
   });
 });
--- src/modules/projectHandler.module.js
+++ src/modules/projectHandler.module.js
@@ -1,21 +1,139 @@
-//
-//
-// Constants
-//
+const { UnavailableError, UnhandledError } = require("@knowdev/errors");
+const { envBoolean } = require("@knowdev/functions");
+const { formatError } = require("@knowdev/http");
+const log = require("@knowdev/log");
 
 //
 //
-// Helper Functions
+// Function Definition
 //
 
-//
-//
-// Main
-//
-
-const projectHandler = () => {
+/**
+ *
+ * @param {Function} handler
+ * @param {Object} options
+ * @param {string} options.name
+ * @returns {Function}
+ */
+function projectHandler(
+  handler,
+  {
+    api = false,
+    name = undefined,
+    unavailable = envBoolean("PROJECT_UNAVAILABLE", { defaultValue: false }),
+    // version = process.env.PROJECT_VERSION,
+  } = {},
+) {
   //
-};
+  //
+  // Validate
+  //
+
+  if (!name) {
+    // If handler has a name, use it
+    if (handler.name) {
+      name = handler.name; // eslint-disable-line no-param-reassign
+    }
+  }
+
+  //
+  //
+  // Setup
+  //
+
+  return (event, context, ...params) => {
+    let response;
+
+    try {
+      log.info.var({ event });
+
+      //
+      //
+      // Preprocess
+      //
+
+      // Check available
+      if (unavailable) {
+        log.warn(
+          "Project unavailable: either PROJECT_UNAVAILABLE=true or { unavailable: true } was passed to projectHandler",
+        );
+        log.debug("Intentionally throwing unavailable");
+        throw UnavailableError();
+      }
+
+      //
+      //
+      // Process
+      //
+
+      // Invoke handler
+      log.trace(`Handler call {name:${name}}`);
+      response = handler(event, context, ...params);
+      log.trace(`Handler exit {name:${name}}`);
+
+      //
+      //
+      // Error Handling
+      //
+    } catch (error) {
+      // if project error
+      if (error.isProjectError) {
+        log.trace("Caught ProjectError");
+        log.var({ projectError: error });
+        // TODO: respond with correct error
+        response = error;
+      } else {
+        // otherwise, respond as unhandled
+        log.trace("Caught unhandled error");
+        log.fatal.var({ unhandledError: error });
+        // TODO: respond with response error
+        response = UnhandledError();
+      }
+    }
+
+    //
+    //
+    // Postprocess
+    //
+
+    // TODO: does response need to conform to API Gateway response?
+    if (api) {
+      log.trace("Formatting response for API Gateway");
+
+      // Format the project error, if there is one
+      if (response.isProjectError) {
+        response = formatError(response); // returns an Express response
+      }
+
+      // Convert status (part of format error) to statusCode
+      if (response.status) {
+        response.statusCode = response.status;
+        delete response.status;
+      }
+
+      // Convert data to body
+      if (response.data) {
+        response.body = response.data;
+        delete response.data;
+      }
+
+      // Stringify body if it is not a string
+      if (typeof response.body !== "string") {
+        response.body = JSON.stringify(response.body);
+      }
+
+      // TODO: Decorate headers?
+    }
+
+    //
+    //
+    // Return
+    //
+
+    log.info.var({ response });
+    return response;
+  };
+}
 
 //
 //
