d68af8d9f0a3067ff1595ec961df0fb97b4bd1a6
Adam Finlayson <amf@adamfinlayson.com>
2023-10-19 21:40:50 -0500

    feat: log init function for tagging

--- src/__tests__/core.spec.js
+++ src/__tests__/core.spec.js
@@ -10,6 +10,40 @@ const core = require("../core");
 // Mock modules
 //
 
+jest.mock("@knowdev/log", () => {
+  // eslint-disable-next-line no-shadow
+  const log = {
+    trace: jest.fn(),
+    debug: jest.fn(),
+    info: jest.fn(),
+    warn: jest.fn(),
+    error: jest.fn(),
+    fatal: jest.fn(),
+    var: jest.fn(),
+    tag: jest.fn(),
+    untag: jest.fn(),
+    with: jest.fn(() => log),
+  };
+  log.trace.var = jest.fn();
+  log.debug.var = jest.fn();
+  log.info.var = jest.fn();
+  log.warn.var = jest.fn();
+  log.error.var = jest.fn();
+  log.fatal.var = jest.fn();
+  // eslint-disable-next-line no-shadow
+  const { LOG_FORMAT, LOG_LEVEL } = jest.requireActual("@knowdev/log");
+  return {
+    Logger: jest.fn(() => log),
+    LOG_FORMAT: {
+      JSON: LOG_FORMAT.JSON,
+    },
+    LOG_LEVEL: {
+      TRACE: LOG_LEVEL.TRACE,
+    },
+    mockFunctions: log,
+  };
+});
+
 //
 //
 // Mock environment
@@ -21,6 +55,7 @@ beforeEach(() => {
 });
 afterEach(() => {
   process.env = DEFAULT_ENV;
+  jest.resetAllMocks();
 });
 
 //
@@ -35,6 +70,15 @@ describe("Core", () => {
   it("Exports the logger", () => {
     expect(core).toHaveProperty("log");
     expect(core.log).toBeObject();
+    expect(core.log.init).toBeFunction();
+    expect(core.log.tag).toBeFunction();
     expect(core.log.var).toBeFunction();
+  });
+  describe("Logger", () => {
+    it("Calls log.tag when init is called", () => {
+      expect(core.log.tag).not.toHaveBeenCalled();
+      core.log.init();
+      expect(core.log.tag).toHaveBeenCalled();
+    });
   });
 });
--- src/core.js
+++ src/core.js
@@ -21,8 +21,50 @@ const log = new Logger({
 
 //
 //
+// Functions
+//
+
+function init({ invoke } = {}) {
+  const tags = {};
+
+  // Commit
+  if (process.env.PROJECT_COMMIT) {
+    tags.commit = process.env.PROJECT_COMMIT;
+  }
+
+  // Environment
+  if (process.env.PROJECT_ENV) {
+    tags.env = process.env.PROJECT_ENV;
+  }
+
+  // Invoke
+  // const invoke = getCurrentInvokeUuid();
+  if (invoke) {
+    tags.invoke = invoke;
+    // Short invoke is first 8 characters
+    tags.shortInvoke = invoke.slice(0, 8);
+  }
+
+  // Project
+  if (process.env.PROJECT_KEY) {
+    tags.project = process.env.PROJECT_KEY;
+  }
+
+  // Version
+  if (process.env.npm_package_version || process.env.PROJECT_VERSION) {
+    tags.version =
+      process.env.npm_package_version || process.env.PROJECT_VERSION;
+  }
+
+  return log.tag(tags); // Currently returning `undefined`
+}
+
+//
+//
 // Export
 //
+
+log.init = init;
 
 module.exports = {
   log,
